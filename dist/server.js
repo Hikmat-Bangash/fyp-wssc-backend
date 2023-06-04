79172d5ce157ae50d36e49e8091c0c04a23caa5c
Hikmat Bangash <hikmatkhanbangash@gmail.com>
2023-06-04 11:25:37 +0500

    updated dist for production

--- dist/server.js
+++ dist/server.js
@@ -36,10 +36,11 @@ mongoose_1.default
     console.log(err);
 });
 //-------- MIDDLEWARES ---------
-app.use((0, cors_1.default)({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+const corsOptions = {
+    origin: 'https://fyp-wsscm-system.vercel.app',
     credentials: true,
-}));
+};
+app.use((0, cors_1.default)(corsOptions));
 app.use((0, helmet_1.default)());
 app.use((0, morgan_1.default)("tiny"));
 app.use((0, cookie_parser_1.default)());
