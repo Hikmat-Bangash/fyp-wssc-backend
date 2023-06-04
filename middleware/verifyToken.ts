77b75ff59a43557ae4f963276e7ebbe670c757d2
Hikmat Bangash <hikmatkhanbangash@gmail.com>
2023-06-04 14:59:55 +0500

    updated

--- middleware/verifyToken.ts
+++ middleware/verifyToken.ts
@@ -13,7 +13,9 @@ export const verifyToken = async (
   next: NextFunction
 ) => {
   const token: any = req.cookies.wssc_token;
- 
+  console.log(`token checking: ${token}`)
+  console.log(`req header: ${req.header}`)
+  
   if (!token) return next(createError(401, "You are not authenticated!"));
 
   return new Promise<void>((resolve, reject) => {
